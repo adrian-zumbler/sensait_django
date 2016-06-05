9d2daba192d91778ca132ecbcb6500b3c0ce679a
Jose Arce <joseangel.epzarce@gmail.com>
2016-06-05 16:38:14 -0500

    Rename ticket detail url

--- ticket/urls.py
+++ ticket/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'dash/tickets/$', TicketListView.as_view(), name='ticket-list'),
     url(r'dash/tickets/add/$', TicketCreateView.as_view(), name='ticket-add'),
     url(r'dash/tickets/edit/(?P<pk>\d+)/$', TicketUpdateView.as_view(), name='ticket-edit'),
-    url(r'dash/tickets/(?P<pk>\d+)/$', TicketDetailView.as_view(), name='ticket-detail'),
+    url(r'dash/tickets/view/(?P<pk>\d+)/$', TicketDetailView.as_view(), name='ticket-detail'),
 ]
