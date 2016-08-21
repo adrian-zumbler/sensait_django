2b50a2c6df1dcf018345e7c4f0f7ca9d800ef986
Jose Arce <joseangel.epzarce@gmail.com>
2016-08-21 18:54:15 -0500

    Sensor type dates

--- arduino/models.py
+++ arduino/models.py
@@ -68,6 +68,9 @@ class SensorType(models.Model):
         null=True
     )
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __unicode__(self):
         return self.name + ': ' + self.description
 
