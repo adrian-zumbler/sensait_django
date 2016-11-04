9c5ae5f2760522d85906e5cf60cb94736d97e378
Jose Arce <joseangel.epzarce@gmail.com>
2016-11-04 07:56:06 -0600

    Improved migration +

--- arduino/migrations/0002_sensordata_epoch.py
+++ arduino/migrations/0002_sensordata_epoch.py
@@ -3,38 +3,6 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-from datetime import timedelta
-
-
-def forwards_func(apps, schema_editor):
-    # We get the model from the versioned app registry;
-    # if we directly import it, it'll be the wrong version
-    Arduino = apps.get_model("arduino", "Arduino")
-    SensorData = apps.get_model("arduino", "SensorData")
-    db_alias = schema_editor.connection.alias
-
-    arduinos = Arduino.objects.using(db_alias).all()
-    for arduino in arduinos:
-        sensors = arduino.arduino_sensors.all()
-        if sensors:
-            esensor = sensors.filter(data_key='field1')
-            if esensor:
-                delta = timedelta(seconds=3)
-                for edata in esensor[0].sensor_data.all():
-                    edatetime = edata.created_at
-                    try:
-                        epoch = int(edata.data) + 18000
-                    except Exception as e:
-                        epoch = 0
-                    SensorData.objects.filter(
-                        arduino_sensor__in=sensors,
-                        created_at__gt=edatetime - delta,
-                        created_at__lt=edatetime + delta
-                    ).update(epoch=epoch)
-
-
-def reverse_func(apps, schema_editor):
-    pass
 
 
 class Migration(migrations.Migration):
@@ -48,6 +16,5 @@ class Migration(migrations.Migration):
             model_name='sensordata',
             name='epoch',
             field=models.PositiveIntegerField(default=0, null=True),
-        ),
-        migrations.RunPython(forwards_func, reverse_func),
+        )
     ]
