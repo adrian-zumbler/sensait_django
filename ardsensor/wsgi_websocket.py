ebe999ea99f6e529066415db8bef78783e95d7bd
Jose Arce <joseangel.epzarce@gmail.com>
2016-11-06 13:20:18 -0600

    Redis before ws4redis

--- ardsensor/wsgi_websocket.py
+++ ardsensor/wsgi_websocket.py
@@ -2,10 +2,7 @@ import os
 import gevent.socket
 import redis.connection
 
-
+redis.connection.socket = gevent.socket
 os.environ.update(DJANGO_SETTINGS_MODULE='ardsensor.settings')
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
-
-
-redis.connection.socket = gevent.socket
 application = uWSGIWebsocketServer()
