5c4c7be40ef3a0931a225b5411c523655aed6a9e
Jose Arce <joseangel.epzarce@gmail.com>
2016-11-06 13:15:32 -0600

    Set settings module var before calling ws4redis

--- ardsensor/wsgi_websocket.py
+++ ardsensor/wsgi_websocket.py
@@ -1,10 +1,11 @@
 import os
 import gevent.socket
 import redis.connection
+
+
+os.environ.update(DJANGO_SETTINGS_MODULE='ardsensor.settings')
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 
 
 redis.connection.socket = gevent.socket
-os.environ.update(DJANGO_SETTINGS_MODULE='ardsensor.settings')
-
 application = uWSGIWebsocketServer()
