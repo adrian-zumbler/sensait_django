adcab2cbc73ec21eaaa852f3e72beb7e41a7c06e
dPeluChe <olimpuz@gmail.com>
2016-12-27 17:13:54 -0600

    Pinshi bug de epoch en el tag....

--- arduino/templatetags/arduino_tags.py
+++ arduino/templatetags/arduino_tags.py
@@ -1,11 +1,21 @@
 from django import template
+from datetime import datetime, time
+from time import time, strftime
 register = template.Library()
+
 
 @register.filter(name="isEven")
 def isEven(idProject):
-    return idProject%2
+    return idProject % 2
 
 
 @register.filter(name="order_by")
 def order_by(queryset, order):
     return queryset.order_by(order)
+
+
+@register.filter(name="epochDate")
+def epochDate(epoch):
+    print epoch
+    # return datetime.fromtimestamp(float(epoch)).strftime('%Y-%m-%d %H:%M:%S')
+    # return datetime.utcfromtimestamp(float(epoch)).strftime('%Y-%m-%dT%H:%M:%SZ')
