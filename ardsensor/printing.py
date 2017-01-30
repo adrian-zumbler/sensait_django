3b00f5be1e0338638934074ecd1f5aa7faf7cd05
dPeluChe <olimpuz@gmail.com>
2017-01-30 17:41:35 -0600

    PDF - Add headers and footers... and page numbers

--- ardsensor/printing.py
+++ ardsensor/printing.py
@@ -2,6 +2,8 @@ from reportlab.lib.pagesizes import letter, A4
 from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.enums import TA_CENTER
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import mm
 from django.contrib.auth.models import User
 
 
@@ -13,6 +15,25 @@ class ReportPrint:
         elif pagesize == 'Letter':
             self.pagesize = letter
         self.width, self.height = self.pagesize
+
+    @staticmethod
+    def _header_footer(canvas, doc):
+        # Save the state of our canvas so we can draw on it
+        canvas.saveState()
+        styles = getSampleStyleSheet()
+
+        # Header
+        header = Paragraph('REPORTE SENSAIT - HEADER .   ' * 5, styles['Normal'])
+        w, h = header.wrap(doc.width, doc.topMargin)
+        header.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin - h)
+
+        # Footer
+        footer = Paragraph('REPORTE SENSAIT - FOOTER   ' * 5, styles['Normal'])
+        w, h = footer.wrap(doc.width, doc.bottomMargin)
+        footer.drawOn(canvas, doc.leftMargin, h)
+
+        # Release the canvas
+        canvas.restoreState()
 
     def print_users(self):
         buffer = self.buffer
@@ -69,4 +90,30 @@ class ReportPrint:
             # elements.append(Paragraph('epoch : %s, data: %s' % (d['epoch'], d['data']), styles['Normal']))
             elements.append(Paragraph('epoch : %s, data: %s' % (data.epoch, data.data), styles['Normal']))
 
-        doc.build(elements)
+        # Se agrega el llamado del header y footer
+        doc.build(elements, onFirstPage=self._header_footer, onLaterPages=self._header_footer, canvasmaker=NumberedCanvas)
+
+
+# Clase para agregar los numeros de las paginas.
+class NumberedCanvas(canvas.Canvas):
+    def __init__(self, *args, **kwargs):
+        canvas.Canvas.__init__(self, *args, **kwargs)
+        self._saved_page_states = []
+
+    def showPage(self):
+        self._saved_page_states.append(dict(self.__dict__))
+        self._startPage()
+
+    def save(self):
+        """add page info to each page (page x of y)"""
+        num_pages = len(self._saved_page_states)
+        for state in self._saved_page_states:
+            self.__dict__.update(state)
+            self.draw_page_number(num_pages)
+            canvas.Canvas.showPage(self)
+        canvas.Canvas.save(self)
+
+    def draw_page_number(self, page_count):
+        # Change the position of this to wherever you want the page number to be
+        self.drawRightString(211 * mm, 15 * mm + (0.2 * inch),
+                             "Page %d of %d" % (self._pageNumber, page_count))
