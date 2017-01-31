d58605a9d276b07512ec2fa74c65bb19ca474fe4
dPeluChe <olimpuz@gmail.com>
2017-01-31 01:13:26 -0600

    REPORTES - Ahi va... pinshi reporte meimmm

--- ardsensor/printing.py
+++ ardsensor/printing.py
@@ -1,10 +1,16 @@
+from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter, A4
-from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.enums import TA_CENTER
+from reportlab.lib.enums import TA_CENTER, TA_RIGHT
 from reportlab.pdfgen import canvas
-from reportlab.lib.units import mm
+from reportlab.lib.units import mm, inch
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from django.conf import settings
 from django.contrib.auth.models import User
+from datetime import datetime
+from sys import getsizeof
 
 
 class ReportPrint:
@@ -23,12 +29,12 @@ class ReportPrint:
         styles = getSampleStyleSheet()
 
         # Header
-        header = Paragraph('REPORTE SENSAIT - HEADER .   ' * 5, styles['Normal'])
+        header = Paragraph('REPORTE SENSAIT - HEADER.', styles['Normal'])
         w, h = header.wrap(doc.width, doc.topMargin)
-        header.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin - h)
+        header.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin - (h - 10))
 
         # Footer
-        footer = Paragraph('REPORTE SENSAIT - FOOTER   ' * 5, styles['Normal'])
+        footer = Paragraph('REPORTE SENSAIT - FOOTER   ', styles['Normal'])
         w, h = footer.wrap(doc.width, doc.bottomMargin)
         footer.drawOn(canvas, doc.leftMargin, h)
 
@@ -38,10 +44,10 @@ class ReportPrint:
     def print_users(self):
         buffer = self.buffer
         doc = SimpleDocTemplate(buffer,
-                                rightMargin=72,
-                                leftMargin=72,
-                                topMargin=72,
-                                bottomMargin=72,
+                                rightMargin=50,
+                                leftMargin=50,
+                                topMargin=60,
+                                bottomMargin=50,
                                 pagesize=self.pagesize)
 
         # Our container for 'Flowable' objects
@@ -58,6 +64,9 @@ class ReportPrint:
         for i, user in enumerate(users):
             elements.append(Paragraph('user id: %d' % user.id, styles['Normal']))
 
+        # for data in report_instance.sensor_data():
+            # elements.append(Paragraph('Fecha : %s, Valor: %s' % (datetime.fromtimestamp(data.epoch).strftime('%d/%m/%Y %H:%M:%S'), data.data), styles['Normal']))
+
         doc.build(elements)
 
         # Get the value of the BytesIO buffer and write it to the response.
@@ -68,10 +77,10 @@ class ReportPrint:
     def print_sensor_data(self, report_instance):
         buffer = self.buffer
         doc = SimpleDocTemplate(buffer,
-                                rightMargin=72,
-                                leftMargin=72,
-                                topMargin=72,
-                                bottomMargin=72,
+                                rightMargin=50,
+                                leftMargin=50,
+                                topMargin=60,
+                                bottomMargin=50,
                                 pagesize=self.pagesize)
 
         # Our container for 'Flowable' objects
@@ -81,14 +90,89 @@ class ReportPrint:
         styles = getSampleStyleSheet()
         styles.add(ParagraphStyle(name='centered', alignment=TA_CENTER))
 
-        # Draw things on the PDF. Here's where the PDF generation happens.
+        # Titulo de la seccion
+        elements.append(Paragraph('Reporte get Tipo de re' + str(report_instance.tipo_reporte), styles['Heading1']))
 
-        # See the ReportLab documentation for the full list of functionality.
+        elements.append(Paragraph('El siguiente reporte abarca el periodo de tiempo de: ' + datetime.fromtimestamp(report_instance.fecha_inicial).strftime('%d/%m/%Y %H:%M:%S') + " al " + datetime.fromtimestamp(report_instance.fecha_final).strftime('%d/%m/%Y %H:%M:%S'), styles['Heading2']))
 
-        elements.append(Paragraph('Sensor data', styles['Heading1']))
-        for data in report_instance.sensor_data():
-            # elements.append(Paragraph('epoch : %s, data: %s' % (d['epoch'], d['data']), styles['Normal']))
-            elements.append(Paragraph('epoch : %s, data: %s' % (data.epoch, data.data), styles['Normal']))
+        elements.append(Paragraph('La informacion que se despliega a continuacion son propiedad de la empresa que contrata el servicio de SENSAIT.', styles['Normal']))
+
+        elements.append(Paragraph('Transmisor' + report_instance.sensor.arduino.name, styles['Normal']))
+
+        elements.append(Paragraph('Projecto' + report_instance.sensor.arduino.project.name, styles['Normal']))
+
+        elements.append(Paragraph('Valor Minimo' + str(report_instance.sensor.min_value), styles['Normal']))
+
+        elements.append(Paragraph('Valor Maximo' + str(report_instance.sensor.max_value), styles['Normal']))
+
+        elements.append(Paragraph('Descripcion' + report_instance.sensor.description, styles['Normal']))
+
+        elements.append(Paragraph('Equipo' + report_instance.sensor.equipment.equipment_name, styles['Normal']))
+
+        elements.append(Paragraph('Encargado' + report_instance.sensor.arduino.project.nombre_encargado, styles['Normal']))
+
+        min_value = report_instance.sensor.min_value
+        max_value = report_instance.sensor.max_value
+        totalAlertas = 0
+        promedioRegistros = 0.0
+        totalRegistros = 0
+
+        for num, data in enumerate(report_instance.sensor_data(), start=0):
+            totalRegistros = num
+            promedioRegistros += float(data.data)
+            if float(data.data) > float(max_value) or float(min_value) > float(data.data):
+                totalAlertas += 1
+
+        elements.append(Paragraph('Promedio' + str(promedioRegistros / totalRegistros), styles['Normal']))
+        elements.append(Paragraph('Total Registros' + str(totalRegistros), styles['Normal']))
+        elements.append(Paragraph('Total Alertas' + str(totalAlertas), styles['Normal']))
+
+
+
+        # Tabla de ejemplo
+        main_table = []
+        dataTable_L = []
+        dataTable_R = []
+        table_data = []
+
+        # Creamos la MAIN tabla para poner todos los registros.
+
+        # Cargaremos los titulos de la tabla
+        table_data.append(("Fecha y Hora", "Valor Registrado", "Estatus"))
+        sensorStatus = "Correcto"
+        for num, data in enumerate(report_instance.sensor_data(), start=0):
+            if float(data.data) > float(max_value) or float(min_value) > float(data.data):
+                sensorStatus = "Alerta"
+            else:
+                sensorStatus = "Correcto"
+            if num % 2 == 0:
+                dataTable_L.append((datetime.fromtimestamp(data.epoch).strftime('%d/%m/%Y %H:%M:%S'), data.data, sensorStatus))
+            else:
+                dataTable_R.append((datetime.fromtimestamp(data.epoch).strftime('%d/%m/%Y %H:%M:%S'), data.data, sensorStatus))
+
+        if len(dataTable_L) > len(dataTable_R):
+            total = len(dataTable_L) - len(dataTable_R)
+            print total
+            for data in range(0, total):
+                dataTable_R.append(("", "", ""))
+
+        if len(dataTable_R) > len(dataTable_L):
+            total = len(dataTable_R) - len(dataTable_L)
+            for data in range(0, total):
+                dataTable_L.append(("", "", ""))
+
+        table_L = Table(dataTable_L, colWidths=[(doc.width) / 3.0] * 3)
+        table_L.setStyle(TableStyle([('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black), ('BOX', (0, 0), (-1, -1), 0.25, colors.black)]))
+
+        table_R = Table(dataTable_R, colWidths=[(doc.width) / 3.0] * 3)
+        table_R.setStyle(TableStyle([('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black), ('BOX', (0, 0), (-1, -1), 0.25, colors.black)]))
+
+        dataTable_Main = [[table_L, table_R]]
+        table_Main = Table(dataTable_Main, colWidths=[doc.width])
+        table_Main.setStyle(TableStyle([('INNERGRID', (0, 0), (-1, -1), 0.25, colors.red), ('BOX', (0, 0), (-1, -1), 0.25, colors.red)]))
+
+        elements.append(table_L)
+        elements.append(table_R)
 
         # Se agrega el llamado del header y footer
         doc.build(elements, onFirstPage=self._header_footer, onLaterPages=self._header_footer, canvasmaker=NumberedCanvas)
@@ -115,5 +199,5 @@ class NumberedCanvas(canvas.Canvas):
 
     def draw_page_number(self, page_count):
         # Change the position of this to wherever you want the page number to be
-        self.drawRightString(211 * mm, 15 * mm + (0.2 * inch),
-                             "Page %d of %d" % (self._pageNumber, page_count))
+        self.drawRightString(211 * mm, 5 * mm + (0.2 * inch),
+                             "Pagina %d de %d" % (self._pageNumber, page_count))
